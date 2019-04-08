30d5f421443536c72abe8527422fe56fe1bd6d66
Charles Noble <charlesddnoble@gmail.com>
2019-04-08 00:44:41 -0400

    added proof of concept to spikes/Docker/poc

---
+++ spikes/Docker/poc/code.py
@@ -0,0 +1,7 @@
+import time					
+								
+def main():					
+	print("Hello World!\n")	
+								
+if __name__ == "__main__":	
+	main()						
---
+++ spikes/Docker/poc/run.py
@@ -0,0 +1,55 @@
+import os
+import docker
+import time
+
+def handle_container(file_name):
+	client = docker.from_env()
+
+	#create container and detach
+	container = client.containers.run('broncode_python', detach = True)
+	
+	#wait until the container is running
+	while "created" in container.status:
+		container.reload()
+	
+	#TODO: get docker-py to do this...	
+	#we should get docker py to do this...	
+	os.system("docker cp "+file_name+" "+container.id+":/")
+
+	#wait until the container is exited
+	while "running" in container.status:
+		container.reload()
+
+	log = container.logs().decode("utf-8")
+	return(log)
+
+def write_code_file(file_name):
+	code =  "import time					\n" \
+			"								\n" \
+			"def main():					\n" \
+			"	print(\"Hello World!\\n\")	\n" \
+			"								\n" \
+			"if __name__ == \"__main__\":	\n" \
+			"	main()						\n" 
+	out_file = open(file_name,"w")
+	out_file.write(str(code))
+	
+def run_time_trial():
+	reps = 20
+	start_time = time.time()
+	for i in range(0,reps):
+		handle_container()
+	elapsed_time = time.time() - start_time
+	elap = str(elapsed_time/reps)
+	print("Average time to run container is "+elap+" sec\n")
+
+def main():
+	#TODO: consider logging inputs and outputs
+	code_file_name = "code.py"
+	write_code_file(code_file_name)
+	log = handle_container(code_file_name)
+	print(log)
+	#TODO: clean up code.py
+
+if __name__ == "__main__":
+	main()
