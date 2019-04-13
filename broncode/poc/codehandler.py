93a37a4042871825da2b5076dad56582f5c2960b
Charles Noble <charles.d.noble@wmich.edu>
2019-04-13 15:59:32 -0400

    implemented timer into codehandler.py

--- broncode/poc/codehandler.py
+++ broncode/poc/codehandler.py
@@ -9,14 +9,18 @@ class CodeHandler:
         self.code_file_name = code_file_name
         self.image_name = image_name
         self.code = code
+	self.time = 0
         self.write_to_file()
         self.log = self.handle_container()
         self.clean_up()
 
     def handle_container(self):
+	error_msg_time_out = 	"Something went wrong running your code:\n\t
+				 It took too long to execute, so we stopped it!\n"
+	time_out = False
         #TODO: Check returns of docker-py function calls for error
         client = docker.from_env()
-
+	
         #create container and detach
         container = client.containers.run(self.image_name, detach = True)
 
@@ -29,10 +33,17 @@ class CodeHandler:
         os.system("docker cp "+self.code_file_name+" "+container.id+":/")
 
         #wait until the container is exited
-        while "running" in container.status:
+	self.time = time.time()
+        while "running" in container.status and not time_out:
             container.reload()
+	    if time.time() - self.time >= 3.5:
+		time_out = True
+	
+	if time_out:
+		log += error_msg_time_out
+	else:
+        	log = container.logs().decode("utf-8")
 
-        log = container.logs().decode("utf-8")
         return(log)
 
     def write_to_file(self):
