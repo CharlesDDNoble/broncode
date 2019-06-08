0f0fef56c9546c1e563b35278c1e9991ee9ebe14
Charles Noble <charlesddnoble@gmail.com>
2019-06-08 16:01:30 -0400

    added test cases for CodeHandler

--- broncode/poc/tests.py
+++ broncode/poc/tests.py
@@ -1,14 +1,39 @@
 from django.test import TestCase
-from codehandler import CodeHandler
+from .codehandler import CodeHandler
 
 # Create your tests here.
 
 class CodeHandlerTestCase(TestCase):
     def setUp(self):
-        
+        pass
 
     def test_make_block(self):
-        # NO MESSAGE
         handler = CodeHandler(host = '', port = 4000, code = '', flags = '')
-        msg = handler.make_block('')
-        self.assertEqual(len(msg) == handler.BLOCK_SIZE)
+        
+        # NO MESSAGE
+        inp = b''
+        exp = b'\0'*handler.BLOCK_SIZE
+        out = handler.make_block(inp)
+        self.assertEqual(len(out),handler.BLOCK_SIZE)
+        self.assertEqual(out,exp)
+
+        # MESSAGE < BLOCK_SIZE
+        inp = b'Hello'
+        exp = b'Hello' + b'\0'*(handler.BLOCK_SIZE-5)
+        out = handler.make_block(inp)
+        self.assertEqual(len(out),handler.BLOCK_SIZE)
+        self.assertEqual(out,exp)
+
+        # MESSAGE == BLOCK_SIZE
+        inp = b'A'*handler.BLOCK_SIZE
+        exp = b'A'*handler.BLOCK_SIZE
+        out = handler.make_block(inp)
+        self.assertEqual(len(out),handler.BLOCK_SIZE)
+        self.assertEqual(out,exp)
+
+        # MESSAGE > BLOCK_SIZE
+        inp = b'B'*(handler.BLOCK_SIZE+10)
+        exp = b'B'*handler.BLOCK_SIZE
+        out = handler.make_block(inp)
+        self.assertEqual(len(out),handler.BLOCK_SIZE)
+        self.assertEqual(out,exp)
