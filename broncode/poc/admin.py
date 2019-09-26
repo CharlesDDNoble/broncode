2eeafe66415bf38773ae56e3334414c01efdfcb2
Josh Chandler <j5d1c7@gmail.com>
2019-09-26 15:24:06 -0400

    added broncode models

--- broncode/poc/admin.py
+++ broncode/poc/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+import models
 
 # Register your models here.
+admin.site.register(models.Course)
+admin.site.register(models.Chapter)
+admin.site.register(models.Lesson)
+admin.site.register(models.User)
+admin.site.register(models.SolutionSet)
+admin.site.register(models.Submission)
--- broncode/poc/models.py
+++ broncode/poc/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+CODE_MAXLEN = 4096
+FLAGS_MAXLEN = 512
+
+class Course(models.Model):
+    title = models.CharField(max_length=256)
+
+class Chapter(models.Model):
+    title = models.CharField(max_length=128)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=128)
+    chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
+    example_code = models.CharField(max_length=CODE_MAXLEN)
+    compiler_flags = models.CharField(max_length=FLAGS_MAXLEN)
+
+class User(models.Model):
+    username = models.CharField(max_length=64, primary_key=True)
+    password = models.CharField(max_length=128)
+    courses = models.ManyToManyField(Course)
+    owned_courses = models.ManyToManyField(Course)
+    completed_lessons = models.ManyToManyField(Lesson)
+
+class SolutionSet(models.Model):
+    ordering = models.IntegerField
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    stdin = models.CharField(max_length=2048)
+    stdout = models.CharField(max_length=2048)
+
+class Submission(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    code = models.CharField(max_length=CODE_MAXLEN)
+    compiler_flags = models.CharField(max_length=FLAGS_MAXLEN)
+    passed = models.BooleanField(default=False)
