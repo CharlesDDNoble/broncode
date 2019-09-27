12f102ba73ffdb09e4042095e36d814f5f4512d6
Xander Wardell <mascrinthus@gmail.com>
2019-09-27 16:33:37 -0400

    Adapted my code to work with Ahmed's

--- broncode/poc/views.py
+++ broncode/poc/views.py
@@ -6,6 +6,8 @@ from .codehandler import CodeHandler
 import os
 import random
 import string
+from django.http import HttpResponse
+import json
 
 def index(request):
     # Renders the home page.
@@ -40,36 +42,55 @@ def tutorial(request):
     # Renders the home page.
     assert isinstance(request, HttpRequest)
     
-    # Grab sample code
-    filename = finders.find('poc/samplefiles/testCode.c')
-    fp = open(filename, 'r')
-    sampleCode = fp.read()
+    if request.method == 'POST':
+        print(request.POST)
+        code = request.POST.get('codearea','na')
+        compilerFlags = request.POST.get('compileFlags', 'na')
+        log = ''
+        host = ''
+        port = 4000
 
-    # Get variable from template ('component/codemirror.html')
-    code = request.POST.get('codearea','na')
+        # handle the code execution using docker
+        if code != 'na':
+            handler = CodeHandler(host,port,code,compilerFlags)
+            handler.run()
+            log = handler.log
+        else :
+            print("dammit")
+        return HttpResponse(
+            content=json.dumps(log),
+            content_type="application/json"
+        )
+    else:
+        # Grab sample code
+        filename = finders.find('poc/samplefiles/testCode.c')
+        fp = open(filename, 'r')
+        sampleCode = fp.read()
 
-    # Get variable from template ('component/flags.html')
-    compilerFlags = request.POST.get('compileFlags', 'na')
-    log = ''
-    host = ''
-    port = 4000
-    # handle the code execution using docker
-    if code != 'na':
-         handler = CodeHandler(host,port,code,compilerFlags)
-         handler.run()
-         log = handler.log
-         sampleCode = code
+        # Get variable from template ('component/codemirror.html')
+        code = request.POST.get('codearea','na')
 
-
-    # Render the 'index.html' page
-    return render(
-        request,
-        'poc/tutorial.html',
-        {
-            'title': 'Broncode',
-            'year': datetime.now().year,
-            'codeText': sampleCode,
-            'defaultFlags': '-g -O3',
-            'testResult' : log
-        }
-    )
+        # Get variable from template ('component/flags.html')
+        compilerFlags = request.POST.get('compileFlags', 'na')
+        log = ''
+        host = ''
+        port = 4000
+        # handle the code execution using docker
+        if code != 'na':
+            handler = CodeHandler(host,port,code,compilerFlags)
+            handler.run()
+            log = handler.log
+            sampleCode = code
+    
+        # Render the 'index.html' page
+        return render(
+            request,
+            'poc/tutorial.html',
+            {
+                'title': 'Broncode',
+                'year': datetime.now().year,
+                'codeText': sampleCode,
+                'defaultFlags': '-g -O3',
+                'testResult' : log
+            }
+        )
