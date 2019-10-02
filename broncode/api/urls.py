a5c3d4534780bda580842cc685e338545468bf06
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-02 17:00:16 -0400

    add correct url

--- broncode/api/urls.py
+++ broncode/api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='broncode api'),
+    path('users/', views.ListUsersView, name='users-all')
 ]
--- broncode/api/views.py
+++ broncode/api/views.py
@@ -1,6 +1,11 @@
+from Django import HttpResponse
+
 from rest_framework import generics
 from poc.models import User
 from .serializers import UserSerializer
+
+def index(request):
+    return HttpResponse("Welcome to the broncode api :)")
 
 class ListUsersView(generics.ListAPIView):
     """
