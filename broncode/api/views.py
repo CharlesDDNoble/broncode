e0ba5a04651f3eae3c697e8de343ce8962ffe151
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-02 16:56:29 -0400

    test view

--- broncode/api/views.py
+++ broncode/api/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from poc.models import User
+from .serializers import UserSerializer
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello world!")
+class ListUsersView(generics.ListAPIView):
+    """
+    Provides a get method handler.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
