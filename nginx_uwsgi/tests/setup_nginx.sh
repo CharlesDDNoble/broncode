19cd157d0641ddaedfebc5d56c05a3abb4c3203e
Charles Noble <charlesddnoble@gmail.com>
2019-10-04 01:42:54 -0400

    Finished work on configuration scripts to auto configure nginx and uwsgi.

--- nginx_uwsgi/tests/setup_nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo cp ./uwsgi_params /etc/nginx
-sudo cp ./broncode_nginx.conf /etc/nginx/sites-available/
-sudo rm -f /etc/nginx/sites-enabled/broncode_nginx.conf
-sudo ln -s /etc/nginx/sites-available/broncode_nginx.conf /etc/nginx/sites-enabled/
-
-#restart nginx
-sudo /etc/init.d/nginx restart
