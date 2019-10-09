92c290d13d2144c2a7bf844da973fea261c09f34
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 11:29:42 -0400

    nested enrolled_in

--- broncode/api/serializers.py
+++ broncode/api/serializers.py
@@ -2,8 +2,16 @@ from rest_framework import serializers
 
 # from django.contrib.auth.models import User
 from poc.models import User
+from poc.models import Course
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ("id, title")
 
 class UserSerializer(serializers.ModelSerializer):
+    enrolled_in = CourseSerializer(many=True, read_only=True)
+
     class Meta:
         model = User
         fields = ("username","enrolled_in","owned_courses","completed_lessons")
