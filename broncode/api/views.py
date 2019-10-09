cf5e7074e6310b8a31ead904346dd5c285a09297
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 10:13:30 -0400

    use generics

--- broncode/api/views.py
+++ broncode/api/views.py
@@ -1,53 +1,12 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from django.http import Http404
+from rest_framework import generics
 
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 
-class UserList(APIView):
-    """
-    List all users, or create a new user.
-    """
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class UserDetail(APIView):
-    """
-    Retrieve, update, or delete a user
-    """
-    def get_object(self, username):
-        try:
-            return User.objects.get(username=username)
-        except User.DoesNotExist:
-            raise Http404
-
-    def get(self, request, username):
-        user = self.get_object(username)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request, username):
-        user = self.get_object(username)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, username):
-        user = self.get_object(username)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
