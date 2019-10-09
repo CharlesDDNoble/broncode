f5786fe98fbe4b42620d4e75e3816317abd9f685
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 10:40:38 -0400

    many2many

--- broncode/api/serializers.py
+++ broncode/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
+
 from django.contrib.auth.models import User
+from broncode.poc.models import Course
 
 class UserSerializer(serializers.ModelSerializer):
+    owned_courses = CourseSerializer(read_only=True, many=True)
+
     class Meta:
         model = User
         fields = ("username","email","owned_courses")
+
+class CourseSerializer(serializers.ModelSerializer):
+    owners = UserSerializer(many=True)
+    class Meta:
+        model = Course
+        fields = ("title", "owners")
