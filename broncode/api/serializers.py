fb675dfdeb2a7e625d3329d23db2a149c619e123
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 11:32:32 -0400

    course serializer

--- broncode/api/serializers.py
+++ broncode/api/serializers.py
@@ -7,10 +7,11 @@ from poc.models import Course
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = ("id", "title")
+        fields = ("id", "title", "chapters", "lessons")
 
 class UserSerializer(serializers.ModelSerializer):
     enrolled_in = CourseSerializer(many=True, read_only=True)
+    owned_courses = CourseSerializer(many=True, read_only=True)
 
     class Meta:
         model = User
