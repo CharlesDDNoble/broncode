2acfed6ea7d6d796523c2ddbf28a10f3b882e900
Xander Wardell <mascrinthus@gmail.com>
2019-10-09 15:01:26 -0400

    Remove Codemirror and Lessons folders from root

--- broncode/codemirror/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import Http404
-from django.shortcuts import render
-
-def index(request):
-    return render(request, 'codemirror/codemirror.html')
