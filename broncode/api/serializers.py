107e1c3df6430b91e5c9f014ea7906b30d9d5ae3
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 10:42:50 -0400

    ordering

--- broncode/api/serializers.py
+++ broncode/api/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from poc.models import Course
 
+class CourseSerializer(serializers.ModelSerializer):
+    # owners = UserSerializer(many=True)
+    class Meta:
+        model = Course
+        fields = ("title", "owners")
+
 class UserSerializer(serializers.ModelSerializer):
     owned_courses = CourseSerializer(read_only=True, many=True)
 
@@ -10,8 +16,3 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("username","email","owned_courses")
 
-class CourseSerializer(serializers.ModelSerializer):
-    owners = UserSerializer(many=True)
-    class Meta:
-        model = Course
-        fields = ("title", "owners")
