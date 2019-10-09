196f60fbc1157a3145ac3c52b001bbdddb0df9ec
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 11:46:07 -0400

    lightcourseserializer

--- broncode/api/serializers.py
+++ broncode/api/serializers.py
@@ -4,16 +4,21 @@ from rest_framework import serializers
 from poc.models import User
 from poc.models import Course
 
+class LightCourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ("id", "title")
+
+class UserSerializer(serializers.ModelSerializer):
+    enrolled_in = LightCourseSerializer(many=True, read_only=True)
+    owned_courses = LightCourseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ("username","enrolled_in","owned_courses","completed_lessons")
+
 class CourseSerializer(serializers.ModelSerializer):
     owners = UserSerializer(many=True, read_only = False)
     class Meta:
         model = Course
         fields = ("id", "title", "chapters", "lessons", "owners")
-
-class UserSerializer(serializers.ModelSerializer):
-    enrolled_in = CourseSerializer(many=True, read_only=True)
-    owned_courses = CourseSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = User
-        fields = ("username","enrolled_in","owned_courses","completed_lessons")
