4f4bfd11cf9dc036425263a9c8647ba17e0f700e
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 10:07:19 -0400

    many=true

--- broncode/api/views.py
+++ broncode/api/views.py
@@ -14,7 +14,7 @@ class UserList(APIView):
     """
     def get(self, request):
         users = User.objects.all()
-        serializer = UserSerializer(users)
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
     def post(self, request):
