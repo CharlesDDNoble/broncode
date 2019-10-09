e2ac2ce39e9a5f617b13a5aad39ca9a23f0dc30a
Josh Chandler <joshua.d.chandler@wmich.edu>
2019-10-09 10:36:32 -0400

    fixed imports 2

--- broncode/api/views.py
+++ broncode/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from rest_framework import api_view
+from rest_framework.decorators import api_view
 
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
