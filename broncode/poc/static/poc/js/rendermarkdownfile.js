15ad9fb87cc50f43cf3c72b060eff6f45f50c045
Josh Chandler <j5d1c7@gmail.com>
2019-10-25 16:40:58 -0400

    real one :)

--- broncode/poc/static/poc/js/rendermarkdownfile.js
+++ broncode/poc/static/poc/js/rendermarkdownfile.js
@@ -1,26 +1,8 @@
-// see https://dev.to/shoupn/javascript-fetch-api-and-using-asyncawait-47mp
-async function getFileContents(file) {
-    let response = await fetch(file);
-    let contents = await response.text();
-    return contents;
-}
+function renderMarkdown() {
+    let container = $("#instructions")
 
-function renderMarkdownFile(path) {
-    console.log("Rendering " + path);
+    showdown.setFlavor('github');
+    let converter = new showdown.Converter();
 
-    // grabs the html element containing the <script> tag invoking this function
-    let container = document.currentScript.parentElement;
-
-    // grab file contents...
-    getFileContents(path)
-    .then(function(text) {
-        showdown.setFlavor('github');
-        let converter = new showdown.Converter();
-
-        // put in container we grabbed earlier
-        container.innerHTML = converter.makeHtml(text); 
-    },
-    function(text) {
-        console.error("Something went wrong in fetching the contents of " + path);
-    });
+    container.innerHTML = converter.makeHtml(text);
 }
--- spikes/jsmarkdown/rendermarkdownfile.js
+++
@@ -1,8 +0,0 @@
-function renderMarkdown() {
-    let container = $("#instructions")
-
-    showdown.setFlavor('github');
-    let converter = new showdown.Converter();
-
-    container.innerHTML = converter.makeHtml(text);
-}
