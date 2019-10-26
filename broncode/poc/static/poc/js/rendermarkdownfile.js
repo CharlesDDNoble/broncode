7193744247e8ea90f641ec289ddee58865bc5810
Josh <joshua.d.chandler@wmich.edu>
2019-10-26 17:23:40 -0400

    unescapedHTML

--- broncode/poc/static/poc/js/rendermarkdownfile.js
+++ broncode/poc/static/poc/js/rendermarkdownfile.js
@@ -1,9 +1,23 @@
+// turns markdown into html
 function renderMarkdown() {
-    console.log("Rendering markdown...")
+    // console.log("Rendering markdown...")
     let container = $("#instructions")
 
     showdown.setFlavor('github');
     let converter = new showdown.Converter();
 
     container.html(converter.makeHtml(container.html()));
+
+    // fix &lt; and &gt; in code blocks
+    let codes = container.find("code");
+    codes.each(function () {
+        $(this).text(function(index, text) {
+            return unescapeHTML(text);
+        });
+    });
+}
+
+//from https://stackoverflow.com/a/5302113
+function unescapeHTML(escapedHTML) {
+    return escapedHTML.replace(/&lt;/g,'<').replace(/&gt;/g,'>').replace(/&amp;/g,'&');
 }
