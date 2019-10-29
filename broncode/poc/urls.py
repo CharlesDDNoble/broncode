66875300146d7c8eb33393ae1c377973e6fec9fa
Ahmed Radwan <baha3a@yahoo.com>
2019-10-29 14:41:11 -0400

    changed order of url naming

--- broncode/poc/urls.py
+++ broncode/poc/urls.py
@@ -4,7 +4,7 @@ from poc import views
 urlpatterns = [
     path('', views.index, name = 'homepage'),
     path('course/', views.createCourse, name = 'create_course'),
-    path('/lesson/<slug:course_name>', views.createLesson, name = 'create_lesson'),
+    path('lesson/<slug:course_name>/', views.createLesson, name = 'create_lesson'),
     path('lesson/<int:lesson_id>/', views.lesson, name = 'lesson'),
     path('register/', views.register, name = 'register'),
 ]
