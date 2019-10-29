00c81398444367c881b8f8e526f10d7af45e0bf8
Ahmed Radwan <baha3a@yahoo.com>
2019-10-29 15:15:16 -0400

    changed url name to lesson_list

--- broncode/poc/urls.py
+++ broncode/poc/urls.py
@@ -4,7 +4,7 @@ from poc import views
 urlpatterns = [
     path('', views.index, name = 'homepage'),
     path('course/', views.course, name = 'course'),
-    path('course/<int:course_id>', views.lessonList, name = 'course'),
+    path('course/<int:course_id>', views.lessonList, name = 'lesson_list'),
     path('lesson/<int:lesson_id>/', views.lesson, name = 'lesson'),
     path('register/', views.register, name = 'register'),
 ]
