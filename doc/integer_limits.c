5fb1f2c0f0930c23d0a42b8305008a5e5e397937
Charles Noble <32346153+CharlesDDNoble@users.noreply.github.com>
2019-11-15 13:50:06 -0500

    Update integer_limits.c

--- doc/integer_limits.c
+++ doc/integer_limits.c
@@ -12,21 +12,25 @@ int main(int argc, char **argv)
     printf("Maximum value for an object of type char: %d\n\n",CHAR_MAX);
     
     // Shorts
-    printf("Minimum value for an object of type short int: %d\n",SHRT_MIN);
-    printf("Maximum value for an object of type short int: %u\n",SHRT_MAX);
-    printf("Maximum value for an object of type unsigned short int: %u\n\n",USHRT_MAX);
+    printf("Size of short: %lu\n",sizeof(short));
+    printf("Minimum value for an object of type short: %d\n",SHRT_MIN);
+    printf("Maximum value for an object of type short: %u\n",SHRT_MAX);
+    printf("Maximum value for an object of type unsigned short: %u\n\n",USHRT_MAX);
     
     // Ints
+    printf("Size of int: %lu\n",sizeof(int));
     printf("Minimum value for an object of type int: %d\n",INT_MIN);
     printf("Maximum value for an object of type int: %d\n",INT_MAX);
     printf("Maximum value for an object of type unsigned int: %u\n\n",UINT_MAX);
     
     // Longs
+    printf("Size of long: %lu\n",sizeof(long));
     printf("Minimum value for an object of type long int: %ld\n",LONG_MIN);
     printf("Maximum value for an object of type long int: %ld\n",LONG_MAX);
     printf("Maximum value for an object of type unsigned long int: %lu\n\n",ULONG_MAX);
     
     // Long Longs
+    printf("Size of long long: %lu\n",sizeof(long long));
     printf("Minimum value for an object of type long long int: %lld\n",LLONG_MIN);
     printf("Maximum value for an object of type long long int: %lld\n",LLONG_MAX);
     printf("Maximum value for an object of type unsigned long long int: %llu\n\n",ULLONG_MAX);
