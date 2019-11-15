da2e5aa32b1db62d6720292a911524fb24f4481c
Charles Noble <charlesddnoble@gmail.com>
2019-11-15 14:47:37 -0500

    started working on codeserver.py

--- docker-builds/starters/codeserver.py
+++ docker-builds/starters/codeserver.py
@@ -10,7 +10,7 @@ class CodeServer():
     def alarm_handler(self, signum, frame):
         raise TimeoutError('Timeout!')
 
-    def __init__(self,host,port,Executor):
+    def __init__(self,host,port,Executor,code='',flags='',num_inputs='',inputs=[]):
         signal.signal(signal.SIGALRM, self.alarm_handler)
         self.host = host
         self.port = port
@@ -18,6 +18,13 @@ class CodeServer():
         self.max_time = 10
         self.should_end = False
         self.BLOCK_SIZE = 4096 #send/recv message size
+        
+        # for testing
+        self.results_log = ''
+        self.code = code
+        self.flags = code
+        self.inputs = inputs
+        self.num_inputs = num_inputs
 
     def make_block(self,msg):
         """Creates a BLOCK_SIZE message using msg, padding it with \0 if it is too short"""
@@ -30,28 +37,44 @@ class CodeServer():
             has_lost_data = True
         return msg + bytes(('\0' * (self.BLOCK_SIZE-len(msg))),"utf-8")
 
-    def handle_connection(self):
+    def recv_msg(self, connection, is_test, msg=''):
+        if is_test:
+            return msg
+        else:
+            msg = connection.recv(self.BLOCK_SIZE).decode("utf-8").replace('\0','')
+        return msg
+
+    def send_msg(self, connection, is_test, msg=''):
+        if is_test:
+            self.results_log += msg
+        else:
+            connection.send(self.make_block(msg))
+
+    def handle_connection(self, is_test=False):
         #set up server socket
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        serversocket.bind((self.host, self.port))
-        serversocket.listen(1) # become a server socket, maximum 1 connections
+        if not is_test:
+            serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            serversocket.bind((self.host, self.port))
+            serversocket.listen(1) # become a server socket, maximum 1 connections
+
         msg = ''
 
         while not self.should_end:
             try:
                 #block until a connection is made
                 print("waiting...")
-                connection, address = serversocket.accept()
+                if not is_test:
+                    connection, address = serversocket.accept()
                 print("got something.")
 
                 #get compiler flags and code, remove any null bytes from packing
-                flags = connection.recv(self.BLOCK_SIZE).decode("utf-8").replace('\0','')
-                code = connection.recv(self.BLOCK_SIZE).decode("utf-8").replace('\0','')
-                num_inputs = connection.recv(self.BLOCK_SIZE).decode("utf-8").replace('\0','')
+                flags = recv_msg(connection,is_test,self.flags)
+                code = recv_msg(connection,is_test,self.code)
+                num_inputs = recv_msg(connection,is_test,self.num_inputs)
 
                 inputs = []
-                for _ in range(int(num_inputs)):
-                    inputs.append(connection.recv(self.BLOCK_SIZE).decode("utf-8").replace('\0',''))
+                for i in range(int(num_inputs)):
+                    inputs.append(recv_msg(connection,is_test,self.inputs[i]))
 
                 #set alarm to time out in case of really long running programs
                 signal.alarm(self.max_time)
@@ -62,9 +85,10 @@ class CodeServer():
 
                 codex.execute()
                 
-                connection.send(self.make_block(codex.compilation_log))
+                self.send_msg(connection,codex.compilation_log)
+
                 for log in codex.run_logs:
-                    connection.send(self.make_block(log))
+                    self.send_msg(connection,log)
 
                 print("done.")
             except TimeoutError:
--- docker-builds/test.py
+++ docker-builds/test.py
@@ -7,7 +7,8 @@ import client
 from executors.executorstest import CExecutorTest
 from executors.executorstest import PythonExecutorTest
 from executors.executorstest import RExecutorTest
-from client.codeclienttest import CodeClientTest
+#from client.codeclienttest import CodeClientTest
+from starters.codeservertest import CodeServerTest
 
 if __name__ == '__main__':
     unittest.main()
