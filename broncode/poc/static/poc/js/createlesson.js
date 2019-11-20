a97f4fae34f0ea87d1886c5e711cd58ad0043e92
Charles Noble <charlesddnoble@gmail.com>
2019-11-19 20:45:23 -0500

    update lesson-list.html

--- broncode/poc/static/poc/js/createlesson.js
+++ broncode/poc/static/poc/js/createlesson.js
@@ -15,6 +15,7 @@ function delete_lesson(lesson_id) {
         dataType: 'json',
         // handle a successful response
         success : function(json) {
+            M.toast({html: 'Lesson deleted!', classes: 'rounded green lighten-3'});
             $("#lesson_"+lesson_id+"_list").remove();
             $("#lesson_"+lesson_id+"_modal").remove();
             console.log(json);
@@ -22,6 +23,7 @@ function delete_lesson(lesson_id) {
 
         // handle a non-successful response
         error : function(xhr,errmsg,err) {
+            M.toast({html: 'There was an error deleting the lesson!', classes: 'rounded red lighten-3'});
             console.log(xhr.status + ': ' + xhr.responseText); // provide a bit more info about the error to the console
         }
     });
