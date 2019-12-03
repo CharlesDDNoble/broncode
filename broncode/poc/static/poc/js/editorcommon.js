6c2b635e6bd71a7b92e9011c76e4402b1135e240
Charles Noble <charlesddnoble@gmail.com>
2019-12-02 22:09:31 -0500

    modify js for test creation: add csrf code to editorcommon

--- broncode/poc/static/poc/js/editorcommon.js
+++ broncode/poc/static/poc/js/editorcommon.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     }
 });
 
+// FUNCTIONS TO CONTROL TEST CASE BUTTONS
 
 function remove_testcase_row(row) {
     row.slideUp("", function() {
@@ -51,6 +52,8 @@ function add_testcase_row() {
     test_id++;
 }
 
+// END FUNCTIONS TO CONTROL TEST CASE BUTTONS
+
 function validate_input() {
     if ($('#lesson-name').val() == "") {
         M.toast({html: 'Lesson name cannot be blank.'})
@@ -78,6 +81,62 @@ function validate_input() {
     return true;
 }
 
+
+// AJAX API REQUESTS
+$(function() {
+
+    // This function gets cookie with a given name
+    function getCookie(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+    var csrftoken = getCookie('csrftoken');
+
+    /*
+    The functions below will create a header with csrftoken
+    */
+
+    function csrfSafeMethod(method) {
+        // these HTTP methods do not require CSRF protection
+        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    }
+    function sameOrigin(url) {
+        // test that a given url is a same-origin URL
+        // url could be relative or scheme relative or absolute
+        var host = document.location.host; // host + port
+        var protocol = document.location.protocol;
+        var sr_origin = '//' + host;
+        var origin = protocol + sr_origin;
+        // Allow absolute or scheme relative URLs to same origin
+        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
+            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
+            // or any other URL that isn't scheme relative or absolute i.e relative.
+            !(/^(\/\/|http:|https:).*/.test(url));
+    }
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                // Send the token to same-origin, relative URLs only.
+                // Send the token only if the method warrants CSRF protection
+                // Using the CSRFToken value acquired earlier
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+
+});
 
 function post_test_case(idx, jquery_row) {
     test_id = jquery_row.attr("id").split("-")[2];
@@ -179,3 +238,4 @@ function handle_test_cases(lesson_id) {
     }
 }
 
+// END AJAX API REQUESTS
