6bc7bade7d3bb1c200aee767dd94217c9293bcc9
Charles Noble <charlesddnoble@gmail.com>
2019-12-02 22:13:41 -0500

    modify js for test creation: fix api requests

--- broncode/poc/static/poc/js/editorcommon.js
+++ broncode/poc/static/poc/js/editorcommon.js
@@ -83,66 +83,11 @@ function validate_input() {
 
 
 // AJAX API REQUESTS
-$(function() {
-
-    // This function gets cookie with a given name
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-    var csrftoken = getCookie('csrftoken');
-
-    /*
-    The functions below will create a header with csrftoken
-    */
-
-    function csrfSafeMethod(method) {
-        // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-    function sameOrigin(url) {
-        // test that a given url is a same-origin URL
-        // url could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-    }
-
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
-                // Send the token to same-origin, relative URLs only.
-                // Send the token only if the method warrants CSRF protection
-                // Using the CSRFToken value acquired earlier
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-
-});
-
 function post_test_case(idx, jquery_row) {
     test_id = jquery_row.attr("id").split("-")[2];
-    input = jquery_row.find("[id|='command-line']");
-    output = jquery_row.find("[id|='expected']");
-    hint = jquery_row.find("[id|='hint']");
+    input = jquery_row.find("[id|='command-line']").val();
+    output = jquery_row.find("[id|='expected']").val();
+    hint = jquery_row.find("[id|='hint']").val();
 
     return $.ajax({
         url : BRONCODE_URL + '/api/solutionsets/', // the endpoint
@@ -167,9 +112,9 @@ function post_test_case(idx, jquery_row) {
 
 function update_test_case(idx, jquery_row) {
     test_id = jquery_row.attr("id").split("-")[2];
-    input = jquery_row.find("[id|='command-line']");
-    output = jquery_row.find("[id|='expected']");
-    hint = jquery_row.find("[id|='hint']");
+    input = jquery_row.find("[id|='command-line']").val();
+    output = jquery_row.find("[id|='expected']").val();
+    hint = jquery_row.find("[id|='hint']").val();
     
     return $.ajax({
         url :  BRONCODE_URL + "/api/solutionsets/" + test_id + "/",
